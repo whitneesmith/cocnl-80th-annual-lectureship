c883815007f295099b8f18fbf234f5a67bb71a6e
whitneesmith <whitsmit919@gmail.com>
2025-08-16 22:11:33 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 import Home from './pages/Home';
 import Register from './pages/Register';
 import RegisterForm from './pages/RegisterForm';
@@ -11,20 +10,16 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/register-form" element={<RegisterForm />} />
-            <Route path="/hotel" element={<Hotel />} />
-            <Route path="/vendors" element={<Vendors />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register-form" element={<RegisterForm />} />
+          <Route path="/hotel" element={<Hotel />} />
+          <Route path="/vendors" element={<Vendors />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
